1ca6e1b32fd07d55d994492676d8e0928bc9daad
legren <carlosleret@gmail.com>
2021-09-23 18:28:05 +0200

    changed names boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Home from './pages/Home/Home';
+import Tables from './pages/Map/Tables';
+import Profile from './pages/Profile/Profile';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -39,13 +39,13 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <Home />
           </Route>
           <Route exact path="/tab2">
-            <Tab2 />
+            <Tables />
           </Route>
           <Route path="/tab3">
-            <Tab3 />
+            <Profile />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
